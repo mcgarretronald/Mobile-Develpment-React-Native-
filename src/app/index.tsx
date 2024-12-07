8e76cd083313c93c8e0c68d59df28228a755ccad
mcgarretronald <mcgarretronald@gmail.com>
2024-12-08 01:23:14 +0300

    initial commit

---
+++ src/app/index.tsx
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+import Greet from "../components/Greet";
+export default function Index (){
+    return <Greet />
+}
+
+
---
+++ src/components/Greet.tsx
@@ -0,0 +1,6 @@
+import { Text } from "react-native"
+export default function Greet (){
+    return <Text>
+        Good day Mr. React Native Developer
+    </Text>
+}
